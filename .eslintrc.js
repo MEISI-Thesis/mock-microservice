29f828dc9201df8e2058afce7185a45be3387c2f
Diogo <diogorafaellourencooliveira97@gmail.com>
2023-02-27 09:28:38 +0000

    Worked on Ends

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname
+  }
+}
